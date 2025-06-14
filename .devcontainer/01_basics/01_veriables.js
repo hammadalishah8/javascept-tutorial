6f332a1643ebcb64a3f56b0074602efe478c7f9d
hammadalishah8 <ha3873201@gmail.com>
2025-06-14 09:16:05 +0000

    datatypes

--- .devcontainer/01_basics/01_veriables.js
+++ .devcontainer/01_basics/01_veriables.js
@@ -3,6 +3,18 @@ let accountEmail = "hammad@google"
 var accountPassword = "12345"
 accountCity = "Charsadda"
 
-accountId = 2
+// accountId = 2 // not allowed
+
+accountEmail = "hchc@google.com"
+accountPassword = "21212121"
+accountCity = "Peshawar"
+
 
 console.log(accountId);
+
+/*
+prefer not use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// alert(3+3) //we are using nodejs note browser
+
+console.log(3+3)   // code readability should be high
+
+console.log("Hammad")
+
+let name = "Hammad"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/fulse
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); //  undefined
+console.log(typeof null); // object
+
+
