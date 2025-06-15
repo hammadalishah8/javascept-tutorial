b8f92cecc5f22fe305e9446d63bbdfa40654c9a1
hammadalishah8 <ha3873201@gmail.com>
2025-06-15 07:11:30 +0000

    conversion day2 done

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -22,4 +22,28 @@ let isLoggedIn = "Hammad"
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//**********************************  Operations  **********************************
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%10);
+
+// let str1 = "hello "
+// let str2 = "hammad"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
